c94d61e4e81e994d1892a1fbd38b1655ba64b19e
MerajKL5701 <khanmeraj5701@gmail.com>
2023-07-06 15:05:30 +0530

    making it for moblie "hero"

---
+++ anime.js
@@ -0,0 +1,31 @@
+const bg = document.getElementsByClassName("bg")
+const subHero = document.getElementsByClassName("sub-hero")
+const h1 = document.getElementsByClassName("HeroButton")
+// h1[1].style.opacity = "0"
+let bgsupport = 0
+function BGherotranslatefor() {
+    console.log("bgsupoort " + bgsupport + "subhero " + subHero.length);
+    if (bgsupport < subHero.length - 1) {
+        bgsupport++;
+    }
+    if (bgsupport === subHero.length - 1) {
+        h1[0].style.opacity = "0";
+    } else {
+        h1[0].style.opacity = "1";
+        h1[1].style.opacity = "1";
+    }
+    bg[0].style.transform = `translate(${bgsupport * -50}%, 0%)`;
+}
+function BGherotranslateBac() {
+    console.log("bgsupoort " + bgsupport + "subhero " + subHero.length);
+    if (bgsupport > 0) {
+        bgsupport--;
+    }
+    if (bgsupport === 0) {
+        h1[1].style.opacity = "0";
+    } else {
+        h1[1].style.opacity = "1";
+        h1[0].style.opacity = "1";
+    }
+    bg[0].style.transform = `translate(${bgsupport * -50}%, 0%)`;
+}
