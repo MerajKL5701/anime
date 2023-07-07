c2590823aaed368d2659808ec9eb848231a3d31b
MerajKL5701 <khanmeraj5701@gmail.com>
2023-07-07 16:18:24 +0530

    "recommendation and nav"

--- anime.js
+++ anime.js
@@ -1,5 +1,6 @@
 const bg = document.getElementsByClassName("bg")
 const subHero = document.getElementsByClassName("sub-hero")
+const nav = document.querySelector("nav")
 const h1 = document.getElementsByClassName("HeroButton")
 h1[1].style.opacity = "0"
 let bgsupport = 0
@@ -9,8 +10,19 @@ setInterval(() => {
     if (bgsupport >= subHero.length - 1){
         bgsupport = 0
     }
-},4000)
+},8000)
+window.addEventListener("scroll",(() => {
+    const subHeroHeight = parseInt(getComputedStyle(subHero[0]).height)
+    console.log("scroll " + scrollY + " h" + subHeroHeight)
+    if (scrollY > subHeroHeight){
 
+        nav.classList.add("navbar")
+    } else {
+        nav.classList.remove("navbar")
+    }
+}))
+
+// hero bg change 
 
 
 function BGherotranslatefor() {
